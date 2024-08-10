9042205bb70f242f422ea939fb713013a7e67694
buğra uysal <uysalbugra134@gmail.com>
2024-08-10 05:57:10 +0300

    Dünya haritası indirildi, harita işi tamamlandı. Şimdilik veri çekmeye gerek kalmadı.

--- mpclcr.py
+++ mpclcr.py
@@ -1,23 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
 import tkinter as tk
+from PIL import Image, ImageTk
 
 tt=tk.Tk()
 
-tt.geometry("500x800")
-llbl=tk.Label(tt)
+tt.geometry("900x1200")
+
+rsm=Image.open(r"c:\Users\buğra\Desktop\512px-_Political_World__CIA_World_Factbook_map_2005.svg.png")
+
+üi=ImageTk.PhotoImage(rsm)
+
+llbl=tk.Label(tt,image=üi)
 llbl.place(x=20, y=80)
 
-urll="https://ufukavrupa.org.tr/tr/interactive-world-map"
-a=requests.get(urll)
-b=a.content
-c=BeautifulSoup(b,"html.parser")
-
-d=c.find_all('div', class_="jvectormap-container" )
-
-ıımg=[xxx['src']for xxx in d if 'src' in xxx.attrs]
-print(ıımg)
-   
 
 
 

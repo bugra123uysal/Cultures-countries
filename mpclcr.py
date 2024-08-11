512391d9bb8589c8ee26101efc9a96ac1cc6c58e
buğra uysal <uysalbugra134@gmail.com>
2024-08-11 04:56:12 +0300

    iki sayfa ekledim birinde direk ülke isimleri diğer diğer sayfada haritadan ülke seçme olucak

--- mpclcr.py
+++ mpclcr.py
@@ -1,18 +1,53 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+def page_ss(page):  
+    pagea.pack_forget()
+    pageb.pack_forget()
+
+    page.pack()
+
+
+
+
+
 tt=tk.Tk()
 
 tt.geometry("900x1200")
 
-rsm=Image.open(r"c:\Users\buğra\Desktop\512px-_Political_World__CIA_World_Factbook_map_2005.svg.png")
+tr=tk.Label(tt, text="türkiye")
+tr.place(y=1, x=1200)
+almr=tk.Label(tt, text="almanya")
+almr.place(y=22, x=1200)
+
+""" sayfalar """
+
+""" sayfa 1 """
+pagea=tk.Frame(tt)
+syfpage=tk.Label(pagea, text="1. sayfa")
+syfpage.pack()
+abutton=tk.Button(pagea, text="2. sayfaya geç", command=lambda:page_ss(pageb))
+abutton.pack()
+
+
+""" sayfa 2 """
+pageb=tk.Frame(tt)
+syfpage=tk.Label(pageb, text="2. sayfa")
+syfpage.pack()
+
+
+bbutton=tk.Button(pageb, text="1. sayfaya git", command=lambda: page_ss(pagea))
+bbutton.pack()
+
+
+rsm=Image.open(r"c:\Users\buğra\Desktop\Ekran görüntüsü 2024-08-11 024141.png")
 
 üi=ImageTk.PhotoImage(rsm)
 
 llbl=tk.Label(tt,image=üi)
 llbl.place(x=20, y=80)
 
-
+pagea.pack()
 
 
 
